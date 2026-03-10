ee7dddfe6cdc616e5b0b6a03740970d1b707a577
Siddesh B <siddeshb.contact@gmail.com>
2026-03-10 16:39:15 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-317855197-6b47d",
+  "appId": "1:808593703105:web:533a19150b62ac2fa19440",
+  "apiKey": "AIzaSyCu0MhFyK8REud1OXVrF_jLeaE-vBbQHsU",
+  "authDomain": "studio-317855197-6b47d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "808593703105"
+};
