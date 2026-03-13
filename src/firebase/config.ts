de1f5815ea19e8077f0feb2019d7a6f60c5b6a6e
Siddesh B <siddeshb.contact@gmail.com>
2026-03-13 10:49:53 +0000

    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET ( give the key nad variable of this)

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:808593703105:web:533a19150b62ac2fa19440",
   "apiKey": "AIzaSyCu0MhFyK8REud1OXVrF_jLeaE-vBbQHsU",
   "authDomain": "studio-317855197-6b47d.firebaseapp.com",
+  "storageBucket": "studio-317855197-6b47d.firebasestorage.app",
   "measurementId": "",
   "messagingSenderId": "808593703105"
 };
